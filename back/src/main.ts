4333ce07989817227abc2356ada235dc2e3a848d
mateonation <mattfernandezrivera@gmail.com>
2025-04-23 14:57:22 +0200

    global session management + enable CORS

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as session from 'express-session';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.use(
+    session({
+      secret: 'secret-word', // Replace with a strong secret
+      resave: false, // Don't save session if unmodified
+      saveUninitialized: false, // Don't create session until something stored
+      cookie: { secure: false }, // Set to true if using HTTPS
+    }),
+  );
+
+  app.enableCors({
+    origin: 'http://localhost:5173', // frontend URL
+    credentials: true, // Allow cookies to be sent
+  });
+
+  await app.listen(3000);
 }
 bootstrap();
