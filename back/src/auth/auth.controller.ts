1da68db6d69c6b8930862cf9c51547b264900e27
mateonation <mattfernandezrivera@gmail.com>
2025-04-23 15:48:05 +0200

    users functional login endpoint

--- back/src/auth/auth.controller.ts
+++ back/src/auth/auth.controller.ts
@@ -1,4 +1,44 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Req, Res } from '@nestjs/common';
+import { Request, Response } from 'express';
+import { UnauthorizedException } from 'src/errors/unauthorizedException';
+import { LoginUserDto } from 'src/users/dto/login-user.dto';
+import { UsersService } from 'src/users/users.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private readonly usersService: UsersService) {}
+
+    // Endpoint to authenticate user
+    @Post('login')
+    async login(
+        @Body() loginUserDto: LoginUserDto,
+        @Res() res: Response,
+        @Req() req: Request,
+    ){
+        try{
+            // Authenticate user
+            const user = await this.usersService.authenticate(loginUserDto);
+            // Set user in session
+            req.session.user = {
+                id: user.id,
+            };
+            console.log(req.session);
+            // Return 'success' response
+            return res.status(200).json({
+                statusCode: 200,
+                message: 'Logged in',
+                user,
+            });
+        }catch(error){
+            // Handle unauthorized error
+            if (error instanceof UnauthorizedException) {
+                return res.status(error.getStatus()).json(error.getResponse());
+            }
+        }
+        // If error is not handled by service, return 500
+        return res.status(500).json({
+            statusCode: 500,
+            message: 'Internal server error',
+        });
+    }
+}
