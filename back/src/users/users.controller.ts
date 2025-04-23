0bfd7cbf5eb9cb99050012d67ae130bed83c233f
mateonation <mattfernandezrivera@gmail.com>
2025-04-23 13:13:41 +0200

    endpoint that registers users in DB

--- back/src/users/users.controller.ts
+++ back/src/users/users.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Body, Res, Post } from '@nestjs/common';
+import { Response } from 'express';
+import { UsersService } from './users.service';
 
-@Controller('users')
-export class UsersController {}
+@Controller('api/users')
+export class UsersController {
+    constructor(
+        private readonly usersService: UsersService
+    ) {}
+
+    // Endpoint to register a new user
+    @Post('register')
+    async register(
+        @Body() saveUserDto: { username: string; password: string },
+        @Res() res: Response
+    ){
+        try{
+            // Register user in service
+            const user = await this.usersService.save(
+                saveUserDto.username,
+                saveUserDto.password,
+            );
+            // Return 'success' response
+            return res.status(201).json({
+                statusCode: 201,
+                message: 'User registered successfully',
+                user,
+            });
+        } catch (error) {
+            // Handle errors throwed by service
+            return res.status(error.getStatus()).json(error.getResponse());
+        }
+        // If error is not handled by service, return 500
+        return res.status(500).json({
+            statusCode: 500,
+            message: 'Internal server error',
+        });
+    }
+}
