57e154eb6455d9efd9a3dcc94dbddda68046f098
mateonation <mattfernandezrivera@gmail.com>
2025-04-24 16:41:55 +0200

    endpoint returning data of the user in session

--- back/src/users/users.controller.ts
+++ back/src/users/users.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Body, Res, Post } from '@nestjs/common';
-import { Response } from 'express';
+import { Controller, Body, Res, Post, Get, Req } from '@nestjs/common';
+import { Request, Response } from 'express';
 import { UsersService } from './users.service';
 import { SaveUserDto } from './dto/save-user.dto';
 import { ConflictException } from 'src/errors/conflictException';
 import { NotFoundException } from 'src/errors/notFoundException';
+import { UnauthorizedException } from 'src/errors/unauthorizedException';
 
 @Controller('api/users')
 export class UsersController {
@@ -29,6 +30,36 @@ export class UsersController {
         } catch (error) {
             // Handle conflict error
             if (error instanceof ConflictException || error instanceof NotFoundException) {
+                return res.status(error.getStatus()).json(error.getResponse());
+            }
+        }
+        // If error is not handled by service, return 500
+        return res.status(500).json({
+            statusCode: 500,
+            message: 'Internal server error',
+        });
+    }
+
+    // Endpoint to get the info of the logged in user
+    @Get('me')
+    async getLoggedUser(
+        @Res() res: Response,
+        @Req() req: Request,
+    ){
+        try{
+            // If user not logged in, throw exception
+            if (!req.session.user) {
+                throw new UnauthorizedException('You have to be logged in to access this resource');
+            }
+            // Get user info from service
+            const user = await this.usersService.findById(req.session.user.id);
+            // Return 'success' response
+            return res.status(200).json({
+                statusCode: 200,
+                user,
+            });
+        } catch (error) {
+            if (error instanceof UnauthorizedException || error instanceof NotFoundException) {
                 return res.status(error.getStatus()).json(error.getResponse());
             }
         }
