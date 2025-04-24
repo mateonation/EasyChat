2e0233a4dab63bea640863f924cc61907e211937
mateonation <mattfernandezrivera@gmail.com>
2025-04-24 16:46:28 +0200

    user roles as an array of strings

--- back/src/users/dto/user-response.dto.ts
+++ back/src/users/dto/user-response.dto.ts
@@ -1,14 +1,17 @@
+import { Role } from "../role/role";
 import { User } from "../user.entity";
 
 export class UserResponseDto {
     id: number;
     username: string;
     registerDate: Date;
+    roles: string[];
 
     constructor(user: User) {
         this.id = user.id;
         this.username = user.username;
         this.registerDate = user.registerDate;
+        this.roles = user.roles?.map((role: Role) => role.name) || [];
     }
 
     static fromUser(user: User): UserResponseDto {
