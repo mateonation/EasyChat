9a2900e9c282c0f160da51c1acf63662ad362b35
mateonation <mattfernandezrivera@gmail.com>
2025-04-24 13:39:28 +0200

    roles in auth module

--- back/src/auth/auth.module.ts
+++ back/src/auth/auth.module.ts
@@ -3,11 +3,12 @@ import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/users/user.entity';
 import { UsersService } from 'src/users/users.service';
+import { Role } from 'src/users/role/role';
 
 @Module({
   providers: [UsersService],
   exports: [UsersService],
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, Role])],
   controllers: [AuthController]
 })
 export class AuthModule {}
