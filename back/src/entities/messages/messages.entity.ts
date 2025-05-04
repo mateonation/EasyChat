8a66b72a26a786156edbf96e5f1ee23f50e85180
mateonation <mattfernandezrivera@gmail.com>
2025-05-04 12:47:40 +0200

    sender and chat relationships to message entity

--- back/src/entities/messages/messages.entity.ts
+++ back/src/entities/messages/messages.entity.ts
@@ -1,9 +1,22 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { User } from "../users/user.entity";
+import { Chat } from "../chats/chats.entity";
 
 @Entity()
 export class Message {
     @PrimaryGeneratedColumn()
     id: number;
+
+    @ManyToOne(() => User, (user) => user.id, { onDelete: "CASCADE" })
+    senderId: number; // User ID of the sender
+
+    @ManyToOne(() => Chat, (chat) => chat.id, { onDelete: "CASCADE" })
+    chatId: number; // Chat ID of the chat where the message was sent
+
+    @Column({
+        default: false,
+    })
+    isDeleted: boolean; // Indicates if the message is deleted
 
     @Column({
         type: "text",
