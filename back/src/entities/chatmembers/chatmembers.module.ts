86fa7f11c58cf732caf3b5080ff15f25a2a0a1f0
mateonation <mattfernandezrivera@gmail.com>
2025-05-08 10:59:02 +0200

    feat: integrate TypeORM for Chat, ChatMember, and User entities in modules

--- back/src/entities/chatmembers/chatmembers.module.ts
+++ back/src/entities/chatmembers/chatmembers.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ChatmembersService } from './chatmembers.service';
 import { ChatmembersController } from './chatmembers.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ChatMember } from './chatmember.entity';
+import { Chat } from '../chats/chat.entity';
+import { User } from '../users/user.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ChatMember, Chat, User])],
   providers: [ChatmembersService],
-  controllers: [ChatmembersController]
+  controllers: [ChatmembersController],
+  exports: [TypeOrmModule],
 })
 export class ChatmembersModule {}
--- back/src/entities/chats/chats.module.ts
+++ back/src/entities/chats/chats.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ChatsService } from './chats.service';
 import { ChatsController } from './chats.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Chat } from './chat.entity';
+import { ChatMember } from '../chatmembers/chatmember.entity';
+import { User } from '../users/user.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Chat, ChatMember, User])],
   providers: [ChatsService],
-  controllers: [ChatsController]
+  controllers: [ChatsController],
+  exports: [TypeOrmModule],
 })
 export class ChatsModule {}
