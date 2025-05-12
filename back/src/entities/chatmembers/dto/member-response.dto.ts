92d78a8f705cdc8b4d0b7b1f12f922005328e82b
mateonation <mattfernandezrivera@gmail.com>
2025-05-12 16:38:47 +0200

    enhance DTOs for chat and member responses with constructors and static methods for better data handling

--- back/src/entities/chatmembers/dto/member-response.dto.ts
+++ back/src/entities/chatmembers/dto/member-response.dto.ts
@@ -4,4 +4,20 @@ export class MemberResponseDto {
     role: string;
     joinDate: Date;
     user: UserResponseDto;
+
+    constructor(role: string, joinDate: Date, user: UserResponseDto) {
+        this.role = role;
+        this.joinDate = joinDate;
+        this.user = user;
+    }
+    static fromMember(member: any): MemberResponseDto {
+        return new MemberResponseDto(
+            member.role,
+            member.joinDate,
+            UserResponseDto.fromUser(member.user),
+        );
+    }
+    static fromMembers(members: any[]): MemberResponseDto[] {
+        return members.map(member => MemberResponseDto.fromMember(member));
+    }
 }
--- back/src/entities/chats/dto/chat-response.dto.ts
+++ back/src/entities/chats/dto/chat-response.dto.ts
@@ -1,4 +1,5 @@
 import { MemberResponseDto } from "src/entities/chatmembers/dto/member-response.dto";
+import { Chat } from "../chat.entity";
 
 export class ChatResponseDto {
     id: number;
@@ -7,4 +8,21 @@ export class ChatResponseDto {
     name: string;
     groupDescription: string;
     members: MemberResponseDto[];
+
+    constructor(chat: Chat) {
+        this.id = chat.id;
+        this.creationDate = chat.creationDate;
+        this.isGroup = chat.isGroup;
+        this.name = chat.name;
+        this.groupDescription = chat.groupDescription;
+        this.members = MemberResponseDto.fromMembers(chat.members);
+    }
+
+    static fromChat(chat: Chat): ChatResponseDto {
+        return new ChatResponseDto(chat);
+    }
+
+    static fromChats(chats: Chat[]): ChatResponseDto[] {
+        return chats.map(chat => new ChatResponseDto(chat));
+    }
 }
