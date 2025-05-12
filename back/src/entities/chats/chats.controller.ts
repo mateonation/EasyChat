0a2e19105029bc34599f466603c188446bdbb7f3
mateonation <mattfernandezrivera@gmail.com>
2025-05-12 20:29:26 +0200

    separate logic from chat creation and user addition

--- back/src/entities/chats/chats.controller.ts
+++ back/src/entities/chats/chats.controller.ts
@@ -7,6 +7,7 @@ import { ForbiddenException } from 'src/errors/forbiddenException';
 import { BadRequestException } from 'src/errors/badRequestException';
 import { NotFoundException } from 'src/errors/notFoundException';
 import { UsersService } from '../users/users.service';
+import { ChatmembersService } from '../chatmembers/chatmembers.service';
 
 @UseGuards(RolesGuard)
 @Controller('api/chats')
@@ -14,6 +15,7 @@ export class ChatsController {
     constructor(
         private readonly chatsService: ChatsService,
         private readonly usersService: UsersService,
+        private readonly membersService: ChatmembersService, 
     ) {}
 
     // Endpoint to create an individual chat between two users
@@ -27,23 +29,27 @@ export class ChatsController {
         try {
             // Check if the requester and other user exist
             if(!req.session.user?.id) return;
-            const requesterId = req.session.user?.id;
-            console.log('Requester ID:', requesterId);
-            console.log('Other User ID:', otherUserId);
+            const requesterId = req.session.user.id;
             if (!requesterId || !(await this.usersService.findById(requesterId)) || !(await this.usersService.findById(otherUserId))) throw new NotFoundException('One or both users not found.');
             // Throw bad request exception if the user tries to create a chat with themselves
             if(requesterId == otherUserId) throw new BadRequestException('You cannot create a chat with yourself');
             // Check if the chat already exists
             const existingChat = await this.chatsService.findIndividualChat(requesterId, otherUserId);
-            if (existingChat) {
+            if (existingChat) { // If chat already exists, return it
                 return res.status(200).json({
                     statusCode: 200,
                     message: 'Chat already exists',
                     chat: existingChat,
                 });
             }
-            // Create chat
-            const chat = await this.chatsService.createIndividualChat(requesterId, otherUserId);
+            // If not, create a new chat
+            const chatCreated = await this.chatsService.createChat();
+            if (!chatCreated) return;
+            // Add both users to the chat
+            await this.membersService.addUserToChat(requesterId, chatCreated.id);
+            await this.membersService.addUserToChat(otherUserId, chatCreated.id);
+            // Fetch chat with its members
+            const chat = await this.chatsService.findById(chatCreated.id);
             return res.status(201).json({
                 statusCode: 201,
                 message: 'Chat created successfully',
