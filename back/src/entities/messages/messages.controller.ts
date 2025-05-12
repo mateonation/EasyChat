4b3b8bfb7b9f082ccca5cab028e72cbd40dc6641
mateonation <mattfernandezrivera@gmail.com>
2025-05-12 19:10:56 +0200

    delete endpoint to delete messages

--- back/src/entities/messages/messages.controller.ts
+++ back/src/entities/messages/messages.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post, Req, Res, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Req, Res, UseGuards } from '@nestjs/common';
 import { RolesGuard } from 'src/guards/roles.guard';
 import { Request, Response } from 'express';
 import { MessagesService } from './messages.service';
@@ -35,6 +35,45 @@ export class MessagesController {
                 statusCode: 201,
                 message: 'Message sent successfully',
                 data: message,
+            });
+        } catch (error) {
+            // Handle conflict error
+            if (error instanceof ForbiddenException || error instanceof NotFoundException) {
+                return res.status(error.getStatus()).json(error.getResponse());
+            }
+            // If error is not handled by service, return 500
+            return res.status(500).json({
+                statusCode: 500,
+                message: 'Internal server error',
+            });
+        }
+    }
+
+    @Delete('')
+    @Roles('user')
+    async deleteMessage(
+        @Body('messageId') messageId: number,
+        @Res() res: Response,
+        @Req() req: Request,
+    ) {
+        try {
+            if (!req.session.user?.id) return;
+            const requesterId = req.session.user.id;
+            // Check if message exists
+            const message = await this.messagesService.findById(messageId);
+            // If message does not exist or is already deleted, throw a not found exception
+            if (!message || message.isDeleted) {
+                throw new NotFoundException('Message not found');
+            }
+            // If user requesting the deletion is not the sender of the message, throw an error
+            if (message.senderId !== requesterId) {
+                throw new ForbiddenException('You can only delete your own messages');
+            }
+            // Delete message
+            await this.messagesService.deleteMessage(messageId);
+            return res.status(200).json({
+                statusCode: 200,
+                message: 'Message deleted successfully',
             });
         } catch (error) {
             // Handle conflict error
