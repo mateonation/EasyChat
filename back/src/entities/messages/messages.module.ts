899939cc04105a769a9dd4b06cc8331192de9f7d
mateonation <mattfernandezrivera@gmail.com>
2025-05-12 17:34:28 +0200

    implement message sending functionality with repository integration and response DTO

--- back/src/entities/messages/messages.module.ts
+++ back/src/entities/messages/messages.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { MessagesService } from './messages.service';
 import { MessagesController } from './messages.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ChatMember } from '../chatmembers/chatmember.entity';
+import { Chat } from '../chats/chat.entity';
+import { User } from '../users/user.entity';
+import { Message } from './message.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Message, Chat, ChatMember, User])],
   providers: [MessagesService],
-  controllers: [MessagesController]
+  controllers: [MessagesController],
+  exports: [TypeOrmModule],
 })
 export class MessagesModule {}
