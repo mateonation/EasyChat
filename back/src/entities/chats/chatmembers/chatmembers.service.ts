0c8698839f05dcd3d49ecbbba784348d8d1bac47
mateonation <mattfernandezrivera@gmail.com>
2025-05-13 14:36:34 +0200

    use chat response dto and chat entities for chat members handling

--- back/src/entities/chats/chatmembers/chatmembers.service.ts
+++ back/src/entities/chats/chatmembers/chatmembers.service.ts
@@ -5,34 +5,35 @@ import { ChatMember } from './chatmember.entity';
 import { Chat } from '../../chats/chat.entity';
 import { User } from '../../users/user.entity';
 import { ChatMemberRole } from 'src/types/chat-members-roles';
+import { UserResponseDto } from 'src/entities/users/dto/user-response.dto';
 
 @Injectable()
 export class ChatmembersService {
     constructor(
         @InjectRepository(ChatMember)
         private readonly memberRepo: Repository<ChatMember>,
-        @InjectRepository(Chat)
-        private readonly chatRepo: Repository<Chat>,
         @InjectRepository(User)
         private readonly userRepo: Repository<User>,
     ) {}
 
     // Function to add a user to a chat
     async addUserToChat(
-        userId: number,
-        chatId: number,
-    ) {
-        const member = this.memberRepo.create({ userId, chatId, });
+        user: UserResponseDto,
+        chat: Chat,
+    ): Promise<void> {
+        const userEntity = await this.userRepo.findOne({ where: { id: user.id } });
+        if (!userEntity) return;
+        const member = this.memberRepo.create({ user: userEntity, chat });
         await this.memberRepo.save(member);
     }
 
     // Function to update the role of a user in a chat
     async updateMemberRole(
-        userId: number,
-        chatId: number,
+        user: UserResponseDto,
+        chat: Chat,
         role: ChatMemberRole,
     ) {
-        const member = await this.memberRepo.findOne({ where: { userId, chatId } });
+        const member = await this.memberRepo.findOne({ where: { user: { id: user.id }, chat: { id: chat.id } } });
         if (!member) return null;
         member.role = role;
         await this.memberRepo.save(member);
