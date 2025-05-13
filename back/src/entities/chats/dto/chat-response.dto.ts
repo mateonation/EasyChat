7c62cb2377ace715bc8ca020b39ebfc589b9a7e8
mateonation <mattfernandezrivera@gmail.com>
2025-05-13 11:33:18 +0200

    set chat name for non group chats

--- back/src/entities/chats/dto/chat-response.dto.ts
+++ back/src/entities/chats/dto/chat-response.dto.ts
@@ -18,8 +18,21 @@ export class ChatResponseDto {
         this.members = MemberResponseDto.fromMembers(chat.members);
     }
 
-    static fromChat(chat: Chat): ChatResponseDto {
-        return new ChatResponseDto(chat);
+    static fromChat(chat: Chat, currentUserId?: number): ChatResponseDto {
+        let name: string | undefined = chat.name;
+        // Set the other member's username as the name of the chat if it's not a group chat and the current user ID is provided
+        if (!chat.isGroup && currentUserId) {
+            const otherMember = chat.members.find(m => m.user.id !== currentUserId);
+            name = otherMember?.user.username ?? 'Unknown';
+        }
+        return {
+            id: chat.id,
+            creationDate: chat.creationDate,
+            isGroup: chat.isGroup,
+            name,
+            groupDescription: chat.groupDescription,
+            members: MemberResponseDto.fromMembers(chat.members),
+        };
     }
 
     static fromChats(chats: Chat[]): ChatResponseDto[] {
