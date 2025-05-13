2804c263ea8e91254a7dae7537014a6855867297
mateonation <mattfernandezrivera@gmail.com>
2025-05-13 14:34:38 +0200

    add a preview of the last message sent to a chat

--- back/src/entities/chats/dto/chat-response.dto.ts
+++ back/src/entities/chats/dto/chat-response.dto.ts
@@ -6,7 +6,8 @@ export class ChatResponseDto {
     creationDate: Date;
     isGroup: boolean;
     name: string;
-    groupDescription: string;
+    description: string;
+    lastMessagePreview?: string;
     members: MemberResponseDto[];
 
     constructor(chat: Chat) {
@@ -14,23 +15,47 @@ export class ChatResponseDto {
         this.creationDate = chat.creationDate;
         this.isGroup = chat.isGroup;
         this.name = chat.name;
-        this.groupDescription = chat.groupDescription;
+        this.description = chat.description;
         this.members = MemberResponseDto.fromMembers(chat.members);
     }
 
     static fromChat(chat: Chat, currentUserId?: number): ChatResponseDto {
         let name: string | undefined = chat.name;
+
         // Set the other member's username as the name of the chat if it's not a group chat and the current user ID is provided
         if (!chat.isGroup && currentUserId) {
             const otherMember = chat.members.find(m => m.user.id !== currentUserId);
             name = otherMember?.user.username ?? 'Unknown';
         }
+
+        // Get last message sent (by date)
+        const lastMessage = chat.messages?.length
+            ? [...chat.messages].sort((a, b) => b.sentDate.getTime() - a.sentDate.getTime())[0]
+            : null;
+
+        let lastMessagePreview = '';
+        if (lastMessage) {
+            const sender = lastMessage.user;
+            const senderIsCurrentUser = sender.id === currentUserId;
+
+            if(chat.isGroup) {
+                lastMessagePreview = senderIsCurrentUser
+                    ? `You: ${lastMessage.content}`
+                    : `${sender.username}: ${lastMessage.content}`;
+            } else {
+                lastMessagePreview = senderIsCurrentUser
+                    ? `You: ${lastMessage.content}`
+                    : `${lastMessage.content}`;
+            }
+        }
+
         return {
             id: chat.id,
             creationDate: chat.creationDate,
             isGroup: chat.isGroup,
             name,
-            groupDescription: chat.groupDescription,
+            description: chat.description,
+            lastMessagePreview,
             members: MemberResponseDto.fromMembers(chat.members),
         };
     }
