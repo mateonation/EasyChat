56fba7e14f906c8bd7021115aa118c11caa57996
mateonation <mattfernandezrivera@gmail.com>
2025-05-16 16:10:44 +0200

    simplify member response dto with user's id, username and member's role and joining date

--- back/src/entities/chats/chatmembers/dto/member-response.dto.ts
+++ back/src/entities/chats/chatmembers/dto/member-response.dto.ts
@@ -1,21 +1,19 @@
-import { UserResponseDto } from "src/entities/users/dto/user-response.dto";
+import { ChatMember } from "../chatmember.entity";
 
 export class MemberResponseDto {
-    role: string;
-    joinDate: Date;
-    user: UserResponseDto;
+    id: number; // User's ID
+    username: string; // User's username
+    joinDate: Date; // Date when user joined chat
+    role: string; // Role of user in chat
 
-    constructor(role: string, joinDate: Date, user: UserResponseDto) {
-        this.role = role;
-        this.joinDate = joinDate;
-        this.user = user;
+    constructor(member: ChatMember) {
+        this.id = member.user.id; // id from user
+        this.username = member.user.username; // username from user
+        this.role = member.role; // role from member
+        this.joinDate = member.joinDate; // joinDate from member
     }
     static fromMember(member: any): MemberResponseDto {
-        return new MemberResponseDto(
-            member.role,
-            member.joinDate,
-            UserResponseDto.fromUser(member.user),
-        );
+        return new MemberResponseDto(member);
     }
     static fromMembers(members: any[]): MemberResponseDto[] {
         return members.map(member => MemberResponseDto.fromMember(member));
