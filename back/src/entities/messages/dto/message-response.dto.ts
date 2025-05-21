5a12627080337433e29935b2a885b16a387ccd18
mateonation <mattfernandezrivera@gmail.com>
2025-05-21 10:31:01 +0200

    include type of message in response

--- back/src/entities/messages/dto/message-response.dto.ts
+++ back/src/entities/messages/dto/message-response.dto.ts
@@ -2,20 +2,22 @@ import { Message } from "../message.entity";
 
 export class MessageResponseDto {
     id: number;
-    content: string | null;
     sentDate: Date;
     senderId: number;
     senderUsername: string;
+    type: string;
     isDeleted: boolean;
+    content: string | null;
 
     constructor(message: Message) {
         this.id = message.id;
-        // If the message is deleted, do not show the content
-        this.content = message.isDeleted ? null : message.content;
         this.sentDate = message.sentDate;
         this.senderId = message.user.id;
         this.senderUsername = message.user.username;
+        this.type = message.type;
         this.isDeleted = message.isDeleted;
+        // If the message is deleted, do not show the content
+        this.content = message.isDeleted ? null : message.content;
     }
 
     static fromMessage(message: Message): MessageResponseDto {
