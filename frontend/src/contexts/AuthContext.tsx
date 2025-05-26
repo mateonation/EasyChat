a3a0b5eab45d854c56e1ccec17ec1767ab071e6e
mateonation <mattfernandezrivera@gmail.com>
2025-05-26 12:15:19 +0200

    remove socket management from AuthContext

--- frontend/src/contexts/AuthContext.tsx
+++ frontend/src/contexts/AuthContext.tsx
@@ -2,25 +2,21 @@ import axios from 'axios';
 import { createContext, PropsWithChildren, useContext, useEffect, useState } from 'react';
 import auth from '../api/auth.ts';
 import { User } from '../types/userdata.dto.ts';
-import { io, Socket } from 'socket.io-client';
 
 interface AuthContextType {
     user: User | null;
     login: (email: string, password: string) => Promise<void>;
     logout: () => Promise<void>;
-    socket: Socket | null;
 }
 
 const AuthContext = createContext<AuthContextType>({
     user: null,
     login() { return Promise.resolve(); },
     logout() { return Promise.resolve(); },
-    socket: null,
 });
 
 export const AuthProvider = ({children}: PropsWithChildren<object>) => {
     const [user, setUser] = useState<User | null>(null);
-    const [socket, setSocket] = useState<Socket | null>(null);
 
     useEffect(() => {
         if(user != null) return;
@@ -33,9 +29,6 @@ export const AuthProvider = ({children}: PropsWithChildren<object>) => {
         try{
             const result = await auth.post<{user: User}>('/login', { email, password });
             updateSession(result.data.user);
-            // Initialize socket connection after successful login
-            const newSocket = io(import.meta.env.VITE_BACKEND_URL, { withCredentials: true, });
-            setSocket(newSocket);
         } catch (error) {
             if(axios.isAxiosError(error) && error.response){
                 throw new Error(error.response.data.message); // Send message of the response to the component
@@ -48,11 +41,6 @@ export const AuthProvider = ({children}: PropsWithChildren<object>) => {
         try {
             await auth.post('/logout');
             updateSession(null);
-            // Disconnect socket if it exists
-            if (socket) {
-                socket.disconnect();
-                setSocket(null);
-            }
         } catch (error) {
             if(axios.isAxiosError(error) && error.response){
                 throw new Error(error.response.data.message); // Send message of the response to the component
@@ -67,7 +55,7 @@ export const AuthProvider = ({children}: PropsWithChildren<object>) => {
         if (user === null) localStorage.removeItem("user");
     };
 
-    return <AuthContext.Provider value={{ user, login, logout, socket }}>{children}</AuthContext.Provider>;
+    return <AuthContext.Provider value={{ user, login, logout }}>{children}</AuthContext.Provider>;
 };
 
 export const useAuthContext = () => useContext(AuthContext);
