1c5354687d5303d2c00e17cdf2e100ffcd666f12
mateonation <mattfernandezrivera@gmail.com>
2025-05-27 11:43:16 +0200

    form field trans for en, es, gl

--- frontend/src/utils/i18n.ts
+++ frontend/src/utils/i18n.ts
@@ -12,12 +12,16 @@ const resources = {
             ERR_NETWORK: "Network error",
             ERR_SERVER: "Server error",
 
+            // FORM FIELDS
+            FORM_USERNAME_LABEL: "Username",
+            FORM_USERNAME_PLACEHOLDER: "Enter your username here",
+            FORM_PASSWORD_LABEL: "Password",
+            FORM_PASSWORD_PLACEHOLDER: "Enter your password here",
+            FORM_PASSWORD2_LABEL: "Repeat password",
+            FORM_PASSWORD2_PLACEHOLDER: "Repeat your password here",
+
             // LOGIN PAGE
             LOGIN_PAGE_TITLE: "Log in to your account",
-            LOGIN_FORM_USERNAME: "Username",
-            LOGIN_FORM_USERNAME_PLACEHOLDER: "Enter your username here",
-            LOGIN_FORM_PASSWORD: "Password",
-            LOGIN_FORM_PASSWORD_PLACEHOLDER: "Enter your password here",
             LOGIN_FORM_SUBMIT: "Login",
             LOGIN_FORM_ERROR: "Username or password is incorrect",
             LOGIN_REGISTER_LINK: "Register a new account",
@@ -30,12 +34,16 @@ const resources = {
             ERR_NETWORK: "Error de red",
             ERR_SERVER: "Error del servidor",
 
+            // CAMPOS DE FORMULARIO
+            FORM_USERNAME_LABEL: "Nombre de usuario",
+            FORM_USERNAME_PLACEHOLDER: "Introduce tu nombre de usuario",
+            FORM_PASSWORD_LABEL: "Contraseña",
+            FORM_PASSWORD_PLACEHOLDER: "Introduce tu contraseña",
+            FORM_PASSWORD2_LABEL: "Confirmar contraseña",
+            FORM_PASSWORD2_PLACEHOLDER: "Introduce tu contraseña de nuevo",
+
             // PÁGINA DE INICIAR SESIÓN
             LOGIN_PAGE_TITLE: "Iniciar sesión",
-            LOGIN_FORM_USERNAME: "Nombre de usuario",
-            LOGIN_FORM_USERNAME_PLACEHOLDER: "Escribe tu nombre de usuario",
-            LOGIN_FORM_PASSWORD: "Contraseña",
-            LOGIN_FORM_PASSWORD_PLACEHOLDER: "Introduce tu contraseña",
             LOGIN_FORM_SUBMIT: "Iniciar sesión",
             LOGIN_FORM_ERROR: "El nombre de usuario o la contraseña son incorrectos",
             LOGIN_REGISTER_LINK: "Crear una cuenta",
@@ -48,12 +56,16 @@ const resources = {
             ERR_NETWORK: "Erro da red",
             ERR_SERVER: "Erro do servidor",
 
+            // CAMPOS DO FORMULARIO
+            FORM_USERNAME_LABEL: "Nome de usuario",
+            FORM_USERNAME_PLACEHOLDER: "Escribe o teu nome de usuario",
+            FORM_PASSWORD_LABEL: "Contrasinal",
+            FORM_PASSWORD_PLACEHOLDER: "Escribe o teu contrasinal",
+            FORM_PASSWORD2_LABEL: "Confirmar contrasinal",
+            FORM_PASSWORD2_PLACEHOLDER: "Escribe o teu contrasinal unha vez máis",
+
             // PÁXINA DE INICIAR SESIÓN
             LOGIN_PAGE_TITLE: "Iniciar sesión",
-            LOGIN_FORM_USERNAME: "Nome de usuario",
-            LOGIN_FORM_USERNAME_PLACEHOLDER: "Escribe o teu nome de usuario",
-            LOGIN_FORM_PASSWORD: "Contrasinal",
-            LOGIN_FORM_PASSWORD_PLACEHOLDER: "Introduce o teu contrasinal",
             LOGIN_FORM_SUBMIT: "Iniciar sesión",
             LOGIN_FORM_ERROR: "O nome de usuario ou o contrasinal non son correctos",
             LOGIN_REGISTER_LINK: "Rexistrar unha conta",
