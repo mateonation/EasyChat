e87aebd50c4f0e2b90ea548a368026fdb8d2c719
mateonation <mattfernandezrivera@gmail.com>
2025-05-29 12:45:03 +0200

    format date of last message sent in different cases

--- frontend/src/components/chatItem/chatItem.tsx
+++ frontend/src/components/chatItem/chatItem.tsx
@@ -1,5 +1,12 @@
-import { Avatar, ListItem, ListItemAvatar, ListItemIcon, ListItemText } from "@mui/material";
+import { 
+    Avatar, 
+    ListItem, 
+    ListItemAvatar, 
+    ListItemIcon, 
+    ListItemText,
+} from "@mui/material";
 import { useNavigate } from "react-router-dom";
+import { t } from "i18next";
 
 const BASE = import.meta.env.VITE_BASE_PATH;
 
@@ -19,6 +26,39 @@ const ChatItem = ({ chat }: ChatItemProps) => {
         navigate(`${BASE}/chats/${chat.id}`);
     };
 
+
+    // Format the date of the last message based on the difference
+    const formatDate = (isoDate: string) => {
+        const now = new Date();
+        const date = new Date(isoDate);
+        const diffMs = now.getTime() - date.getTime();
+        const diffMinutes = Math.floor(diffMs / (1000 * 60));
+        const diffHours = Math.floor(diffMinutes / 60);
+        const diffDays = Math.floor(diffHours / 24);
+
+        // If the difference is less than a minute, return "Now"
+        if (diffMinutes < 1) return t('DATE_NOW');
+
+        // If it's sent in the same day, return the time in "HH:MM" format
+        const sameDay = now.toDateString() === date.toDateString();
+        if (sameDay) return date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+
+        // If it was sent more than 24 hours ago, return "Yesterday"
+        if (diffDays === 1) return t('DATE_YESTERDAY');
+
+        // If it was sent within the last 6 days, return the day of the week
+        if (diffDays <= 6) {
+            return date.toLocaleDateString(undefined, { weekday: 'long' });
+        }
+
+        // If it was sent in the last year, return the date in "MM/DD/" format
+        if (date.getFullYear() === now.getFullYear()) {
+            return `${date.getDate().toString().padStart(2, "0")}/${(date.getMonth() + 1).toString().padStart(2, "0")}`;
+        }
+
+        // For older dates, return the full date in "MM/DD/YY" format
+        return `${date.getDate().toString().padStart(2, "0")}/${(date.getMonth() + 1).toString().padStart(2, "0")}/${date.getFullYear().toString().slice(2)}`;
+    }
     return (
         <ListItem
             component="li"
@@ -40,7 +80,7 @@ const ChatItem = ({ chat }: ChatItemProps) => {
                     <>
                         <strong>{chat.lastMessagePrefix}:</strong> {chat.lastMessageContent}
                         <br />
-                        <span style={{ fontSize: '0.8rem', color: 'gray' }}>{new Date(chat.lastMessageSentDate).toLocaleString()}</span>
+                        <span style={{ fontSize: '0.8rem', color: 'gray' }}>{formatDate(chat.lastMessageSentDate)}</span>
                     </>
                 }
             />
