e49bf93fdd7e7d760f99ca70daf910fb8a320b56
mateonation <mattfernandezrivera@gmail.com>
2025-06-02 13:58:24 +0200

    replace hardcoded user id with the actual id of the user in session WIP

--- frontend/src/pages/chatpagewrapper.tsx
+++ frontend/src/pages/chatpagewrapper.tsx
@@ -1,16 +1,17 @@
 import { useParams } from "react-router-dom";
 import ChatPage from "./chatpage";
 
-const sessionUserId = 123; // REPLACE WITH ACTUAL SESSION USER ID
-
 const ChatPageWrapper = () => {
     const { chatId } = useParams();
+    // Get user from localStorage
+    const user = localStorage.getItem("user") ? JSON.parse(localStorage.getItem("user")!) : null;
 
     const parsedChatId = Number(chatId);
 
     if (isNaN(parsedChatId)) return <div>Chat inválido</div>;
+    if (!user?.id) return <div>Auth needed</div>; // If there's no user saved in localStorage, we assume the user is not authenticated.
 
-    return <ChatPage chatId={parsedChatId} sessionUserId={sessionUserId} />;
+    return <ChatPage chatId={parsedChatId} sessionUserId={user.id} />;
 }
 
 export default ChatPageWrapper;
