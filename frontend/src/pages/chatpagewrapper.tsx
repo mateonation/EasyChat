ee982c601f9477b51c8d18d3d37a36e4d3f9a913
mateonation <mattfernandezrivera@gmail.com>
2025-06-06 13:23:30 +0200

    fixed chat page wrapper handling of errors by showing a modal

--- frontend/src/pages/chatpagewrapper.tsx
+++ frontend/src/pages/chatpagewrapper.tsx
@@ -3,10 +3,9 @@ import ChatPage from "./chatpage";
 import { useAuthContext } from "../contexts/AuthContext";
 import api from "../api/axios";
 import { ChatDto } from "../types/chat.dto";
-import { ErrorResponse } from "../types/errorResponse.interface";
 import { useTranslation } from 'react-i18next';
 import { useEffect, useState } from "react";
-import { Box, CircularProgress } from "@mui/material";
+import { Box, Button, CircularProgress, Dialog, DialogActions, DialogContent, DialogTitle, Typography } from "@mui/material";
 
 const BASE = import.meta.env.VITE_BASE_PATH;
 
@@ -18,11 +17,12 @@ const ChatPageWrapper = () => {
 
     const [chat, setChat] = useState<ChatDto | null>(null);
     const [loading, setLoading] = useState(true);
+    const [errorType, setErrorType] = useState<null | number>(null);
 
     useEffect(() => {
         // Check if user is logged in
-        const user = localStorage.getItem("user");
-        if (!user) {
+        const loggedUser = localStorage.getItem("user");
+        if (!loggedUser) {
             navigate(`${BASE}/login`, { replace: true });
             return;
         }
@@ -39,45 +39,84 @@ const ChatPageWrapper = () => {
             try {
                 const res = await api.get<ChatDto>(`/chats/${parsedChatId}`);
                 setChat(res.data);
-            } catch (error: unknown) {
-                if (error !== null && typeof error === "object" && "message" in error) {
-                    const reason = (error as ErrorResponse).message;
-                    console.error(reason);
-                    switch (reason) {
-                        case `Chat with ID ${chatId} not found`:
-                            alert(t('DOES_NOT_EXIST', { field: t('CHAT') }));
-                            break;
-                        case `You are not a member of this chat`:
-                            alert(t('YOU_ARE_NOT_MEMBER'));
-                            break;
-                        default:
-                            alert(t('ERR_UNEXPECTED'));
-                            break;
-                    }
+            } catch (err: any) {
+                if (err.response) {
+                    setErrorType(err.response.status); // Set error type based on response status
+                    console.error(err.response.data.message);
+                } else {
+                    setErrorType(500); // Internal Server Error
                 }
-                navigate(`${BASE}/chats`, { replace: true });
             } finally {
                 setLoading(false);
             }
         };
 
         fetchChat();
-    }, [chatId, navigate, t, user]);
+    }, [chatId]);
 
-    if (loading || !chat || !user) return (
-        <Box
-            display="flex"
-            justifyContent="center"
-            alignItems="center"
-            height="100vh"
-        >
-            <CircularProgress />
-        </Box>
-    );
+    if (loading) {
+        return (
+            <Box
+                display="flex"
+                flexDirection="column"
+                justifyContent="center"
+                alignItems="center"
+                height="100vh"
+            >
+                <CircularProgress />
+                {t('GENERIC_MSG_LOADING')}
+            </Box>
+        );
+    };
 
-    return (
-        <ChatPage chatId={chat.id} sessionUserId={user.id} onChatInfo={chat} />
-    );
+    if (errorType) {
+        return (
+            <Dialog
+                open={true}
+                onClose={() => { }}
+                disableEscapeKeyDown
+                maxWidth="sm"
+                fullWidth
+            >
+                <DialogTitle>
+                    {errorType === 404 ?
+                        t('GENERIC_MSG_NOT_FOUND')
+                        : errorType === 403 ?
+                            t('GENERIC_MSG_FORBIDDEN')
+                            : t('ERR_UNEXPECTED')
+                    }
+                </DialogTitle>
+                <DialogContent>
+                    <Typography variant="body1">
+                        {errorType === 404 ?
+                            t('DOES_NOT_EXIST', {
+                                field: t('CHAT'),
+                            })
+                            : errorType === 403 ?
+                                t('YOU_ARE_NOT_MEMBER')
+                                : t('FAILED_TO_FETCH_DETAILS', {
+                                    field: t('CHAT'),
+                                })
+                        }
+                    </Typography>
+                </DialogContent>
+                <DialogActions>
+                    <Button
+                        variant="text"
+                        onClick={() => navigate(`${BASE}/chats`, { replace: true })}
+                    >
+                        {t('GENERIC_ANSWER_OK')}
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        );
+    }
+
+    if (chat && user) {
+        return (
+            <ChatPage chatId={chat.id} sessionUserId={user.id} onChatInfo={chat} />
+        );
+    }
 }
 
 
